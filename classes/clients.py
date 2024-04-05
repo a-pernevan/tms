5920084e3bcdb06527e5d6f3870689cc5b6891cb
Andrei Pernevan <apernevan@outlook.com>
2024-04-05 06:54:02 +0300

    classes tests

--- classes/clients.py
+++ classes/clients.py
@@ -1,18 +1,23 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
+from classes.add_vehicle import Vehicule
 
 class Clients:
     def __init__(self, root):
-        main_window = ttk.Notebook(root)
-        main_window.pack(pady=0)
+        self.main_window = ttk.Notebook(root)
+        self.main_window.pack(pady=0)
 
-        main_frame = Frame(main_window, width=900, height=600)
-        client_frame = Frame(main_window, width=900, height=600)
+        self.main_frame = Frame(self.main_window, width=900, height=600)
+        self.client_frame = Frame(self.main_window, width=900, height=600)
 
-        main_frame.pack(fill=BOTH, expand=1)
-        client_frame.pack(fill=BOTH, expand=1)
+        self.main_frame.pack(fill=BOTH, expand=1)
+        self.client_frame.pack(fill=BOTH, expand=1)
 
-        main_window.add(main_frame, text="Principal")
-        main_window.add(client_frame, text="Gestionare clienti")
-        
+        self.main_window.add(self.main_frame, text="Principal")
+        self.main_window.add(self.client_frame, text="Gestionare clienti")
+        self.adaugare_cam = Button(self.main_frame, text="Adaugare", command=self.adaugare)
+        self.adaugare_cam.pack()
+
+    def adaugare(self):
+        Vehicule(self.client_frame)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from tkinter import *
 from tkinter import ttk
 from classes.clients import Clients
+from classes.add_vehicle import Vehicule
 
 root = Tk()
 root.title("TMS Project 2024")
