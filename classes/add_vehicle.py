e807afe3b04d4be3183be3a0a991d41204897ad0
Andrei Pernevan <apernevan@outlook.com>
2024-04-15 13:11:34 +0300

    interfata de introducere vehicul nou

--- classes/add_vehicle.py
+++ classes/add_vehicle.py
@@ -6,6 +6,15 @@ class Vehicule:
         self.client_frame = Frame(root, width=900, height=600)
         self.client_frame.pack(fill=BOTH, expand=1)
 
-        self.window = Frame(self.client_frame, width=900, height=600)
-        self.window.add(self.client_frame, text="Gestionare firme")
-        self.window.pack(fill=BOTH, expand=1)
+        self.vehicule_frame = LabelFrame(self.client_frame, text="Vehicule")
+        self.vehicule_frame.grid(row=0, column=0, sticky="nw")
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Adaugare Vehicul")
+    root.geometry("900x600")
+
+    hello = Vehicule(root)
+
+    root.mainloop()
