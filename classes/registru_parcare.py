66ffdc2f0d18bb7b118ed3c4b07963c843a810bf
Andrei Pernevan <apernevan@outlook.com>
2024-04-29 18:55:27 +0300

    actualizari la registru parcare

--- classes/registru_parcare.py
+++ classes/registru_parcare.py
@@ -6,6 +6,11 @@ from tkinter import messagebox
 class Registru_parcare:
     def __init__(self, master):
         super().__init__()
+
+        # Valori demo pt nr camion
+        self.nr_auto_cap = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Yusen"]
+        self.nr_auto_remorca = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Yusen"]
+
         # Cream interfata si notebook-ul
         # self.master = master
         # self.master.title("Registru parcare")
@@ -25,8 +30,40 @@ class Registru_parcare:
         self.main_window.add(self.samsung_frame, text="Samsung")
         self.main_window.add(self.vizitatori_frame, text="Vizitatori")
 
+        self.logo_frame = Frame(self.tauros_frame)
+        self.logo_frame.pack()
+
+        self.logo_title = Label(self.logo_frame, text="Intrari / Iesiri Tauros", font=("Arial", 20))
+        self.logo_title.pack()
+
+        self.truck_frame = Frame(self.tauros_frame)
+        self.truck_frame.pack()
+
+        self.nr_auto_label = Label(self.truck_frame, text="Nr auto: ")
+        self.nr_auto_label.grid(row=0, column=0)
+
+        self.remorca_label = Label(self.truck_frame, text="Remorca: ")
+        self.remorca_label.grid(row=1, column=0)
+
+        self.nr_auto_combo = ttk.Combobox(self.truck_frame, postcommand=self.search_auto)
+        self.nr_auto_combo.grid(row=0, column=1)
+
+        self.remorca_combo = ttk.Combobox(self.truck_frame, postcommand=self.search_remorca)
+        self.remorca_combo.grid(row=1, column=1)
+
         # self.test_button = Button(self.tauros_frame, text="Test")
         # self.test_button.grid(row=0, column=0)
+
+        # cautare cap tractor
+    def search_auto(self):
+        # global nr_auto_cap
+        self.query = self.nr_auto_combo.get()
+        if self.query:
+            # Perform search based on the query and update the combobox options
+            filtered_values = [value for value in self.nr_auto_cap if query.lower() in value.lower()]
+            self.nr_auto_combo['values'] = filtered_values
+        else:
+            self.nr_auto_combo['values'] = self.nr_auto_cap
 
 # Testam aplicatia
 
