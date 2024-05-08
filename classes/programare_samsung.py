ef99d19dd3bc0cd0e1cfcb1a0d1b85a91beb8b3a
Andrei Pernevan <apernevan@outlook.com>
2024-05-08 22:07:35 +0300

    modul parcare samsung - implementare verificare locuri libere

--- classes/programare_samsung.py
+++ classes/programare_samsung.py
@@ -41,14 +41,14 @@ class Rezervare_parcare:
         self.company_entry = Entry(self.main_window)
         self.company_entry.grid(row=3, column=1, sticky="nw")
 
-        self.parking_space_label = Label(self.main_window, text="Loc parcare alocat:")
-        self.parking_space_label.grid(row=4, column=0, sticky="nw")
+        # self.parking_space_label = Label(self.main_window, text="Loc parcare alocat:")
+        # self.parking_space_label.grid(row=4, column=0, sticky="nw")
 
-        self.parking_space_entry = Entry(self.main_window, state="readonly")
-        self.parking_space_entry.grid(row=4, column=1, sticky="nw")
+        # self.parking_space_entry = Entry(self.main_window, state="readonly")
+        # self.parking_space_entry.grid(row=4, column=1, sticky="nw")
 
-        self.select_place_button = Button(self.main_window, text="Selecteaza loc")
-        self.select_place_button.grid(row=4, column=2, sticky="nw")
+        # self.select_place_button = Button(self.main_window, text="Selecteaza loc")
+        # self.select_place_button.grid(row=4, column=2, sticky="nw")
 
         self.reservation_date_in_label = Label(self.main_window, text="Data intrare:")
         self.reservation_date_in_label.grid(row=5, column=0, sticky="nw")
@@ -62,14 +62,20 @@ class Rezervare_parcare:
         self.reservation_date_out_entry = DateEntry(self.main_window, locale='ro_RO', date_pattern='yyyy-MM-dd')
         self.reservation_date_out_entry.grid(row=6, column=1, sticky="nw")
 
+        self.check_availability_button = Button(self.main_window, text="Verifica disponibilitate", command=self.verifica)
+        self.check_availability_button.grid(row=7, column=1, sticky="nw")
+
+        self.reservation_button = Button(self.main_window, text="Rezervare", state="disabled")
+        self.reservation_button.grid(row=8, column=1, sticky="nw")
+
         date_in = self.reservation_date_in_entry.get().split("-")
         date_out = self.reservation_date_out_entry.get().split("-")
-
+        
         date1 = datetime(int(date_in[0]), int(date_in[1]), int(date_in[2]))
         date2 = datetime(int(date_out[0]), int(date_out[1]), int(date_out[2]))
-
-        print(date1 == date2)
-
+        
+    def verifica(self):
+        date_in_str = str(self.reservation_date_in_entry.get())
         try:
             self.tms_db = mysql.connector.connect(
                 host = os.getenv("HOST"),
@@ -85,19 +91,32 @@ class Rezervare_parcare:
 
         self.my_cursor = self.tms_db.cursor()
 
-        self.my_cursor.execute("SELECT date_out FROM place_1 WHERE date_out > 'date2'  \
-                               UNION ALL SELECT date_out FROM place_2 WHERE date_out > 'date2' ")
-        
+        sql = ("SELECT date_out FROM tauros_park_main WHERE date_out > %s")
+        values = ((date_in_str, ))
+
+        self.my_cursor.execute(sql, values)
         self.result = self.my_cursor.fetchall()
 
-        print(self.result)
+        if self.result:
+            print(len(self.result))
+            print(self.result)
+            if len(self.result) < 20:
+                print(f"Free places: {20 - len(self.result)}")
+            else:
+                print("Parking full")
 
-        for index, date in enumerate(self.result):
-            # print(str(date[0]).split("-"))
-            rez_date = datetime(int(str(date[0]).split("-")[0]), int(str(date[0]).split("-")[1]), int(str(date[0]).split("-")[2]))
-            print(f"Place {index}: {rez_date}")
-            if date2 > rez_date:
-                print(f"Place {index} is available")
+        elif not self.result:
+            print("Parking empty")
+
+        self.my_cursor.close()
+        self.tms_db.close()
+
+        # for index, date in enumerate(self.result):
+        #     # print(str(date[0]).split("-"))
+        #     rez_date = datetime(int(str(date[0]).split("-")[0]), int(str(date[0]).split("-")[1]), int(str(date[0]).split("-")[2]))
+        #     print(f"Place {index}: {rez_date}")
+        #     if date2 > rez_date:
+        #         print(f"Place {index} is available")
 
 
 
